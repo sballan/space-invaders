f64f54f026c6cf4ccf97185746133790fbefb780
Samuel Ballan <sgb4622@gmail.com>
2025-09-20 01:12:47 -0400

    claude checkpoint: remove files

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -5,7 +5,6 @@
 import * as $_404 from "./routes/_404.tsx";
 import * as $_app from "./routes/_app.tsx";
 import * as $api_joke from "./routes/api/joke.ts";
-import * as $debug from "./routes/debug.tsx";
 import * as $greet_name_ from "./routes/greet/[name].tsx";
 import * as $index from "./routes/index.tsx";
 import * as $Counter from "./islands/Counter.tsx";
@@ -18,7 +17,6 @@ const manifest = {
     "./routes/_404.tsx": $_404,
     "./routes/_app.tsx": $_app,
     "./routes/api/joke.ts": $api_joke,
-    "./routes/debug.tsx": $debug,
     "./routes/greet/[name].tsx": $greet_name_,
     "./routes/index.tsx": $index,
   },
